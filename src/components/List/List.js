12a52554cfd4b2b52f61b769d8ad3106279dd823
xlisachan <xlisachan@gmail.com>
2019-01-15 23:51:45 -0500

    Updated add event handler

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListItem from '../ListItem/ListItem';
 import '../List/List.css';
+import { FaPlus } from 'react-icons/fa';
 
 class List extends Component {
     state = {
-        tasks: []
+        tasks: [],
+        userInput: ''
     }
 
     componentDidMount() {
@@ -12,6 +14,31 @@ class List extends Component {
         this.setState({ 
           tasks: data
         })
+    }
+
+    handleUserInput = e => {
+        this.setState({
+            userInput: e.target.value
+        })
+    }
+    
+    add = (e) => {
+        e.preventDefault();
+        this.setState(prevState => ({
+            tasks: [
+                ...prevState.tasks,
+                {
+                    id: this.nextId(),
+                    task: this.state.userInput
+                }
+            ],
+            userInput: ''
+        }))
+    }
+
+    nextId = () => {
+        this.uniqueId = this.uniqueId || 4
+        return this.uniqueId++
     }
 
     update = (newText, i) => {
@@ -32,6 +59,13 @@ class List extends Component {
         return (
             <div className="List">
                 <div className="list-header">To Do List</div>
+                <form className="add-form" onSubmit={ this.add }>
+                    <input type="text" 
+                            onChange={ this.handleUserInput } 
+                            placeholder="Add task to list"
+                            value={ this.state.userInput } />
+                    <button id="add"><FaPlus /></button>
+                </form>
                 <div className="list-body">
                     { this.state.tasks.map(task => {
                         return (
