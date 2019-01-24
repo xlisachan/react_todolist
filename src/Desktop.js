e214f3a42281d61e179e749b3a70b976daa7cc3a
xlisachan <xlisachan@gmail.com>
2019-01-23 22:58:31 -0500

    Created separate component for CSS illustrations

--- src/Desktop.js
+++ src/Desktop.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 import './Desktop.css';
 import List from './components/List/List';
-import { FaGlobeAmericas } from 'react-icons/fa';
+import BackgroundItem from './components/BackgroundItem/BackgroundItem';
 
 class Desktop extends Component {
   render() {
     return (
       <div className="Desktop">
         <List />
-        <div className="background-items">
-          <div className="phone">
-            <div className="phone-camera"></div>
-            <div className="phone-screen"></div>
-            <div className="phone-homebutton"></div>
-          </div>
-          <div className="passport">
-            <div className="passport-book">
-              <div>PASSPORT</div>
-              <div className="passport-globe"><FaGlobeAmericas /></div>
-            </div>
-            <div className="passport-ticket">
-              <div className="passport-stripe"></div>
-            </div>
-          </div>
-        </div>
+        <BackgroundItem />
       </div>
     );
   }
---
+++ src/components/BackgroundItem/BackgroundItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './BackgroundItem.css'
+import { FaGlobeAmericas } from 'react-icons/fa';
+
+class BackgroundItem extends Component {
+  render() {
+    return (
+      <div className="background-items">
+          <div className="phone">
+            <div className="phone-camera"></div>
+            <div className="phone-screen"></div>
+            <div className="phone-homebutton"></div>
+          </div>
+          <div className="passport">
+            <div className="passport-book">
+              <div>PASSPORT</div>
+              <div className="passport-globe"><FaGlobeAmericas /></div>
+            </div>
+            <div className="passport-ticket">
+              <div className="passport-stripe"></div>
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default BackgroundItem;
