6c59c0667be200ca50b258dc1e4b6d8217159de2
xlisachan <xlisachan@gmail.com>
2019-01-30 19:13:49 -0500

    Updated components to stateless functional components

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../stylesheets/Desktop.css';
 import List from './List';
 import BackgroundItem from './BackgroundItem';
 
-class Desktop extends Component {
-  render() {
-    return (
-      <div className="Desktop">
-        <List />
-        <BackgroundItem />
-      </div>
-    );
-  }
-}
+const Desktop = () => (
+  <div className="Desktop">
+    <List />
+    <BackgroundItem />
+  </div>
+);
 
 export default Desktop;
