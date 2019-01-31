fdcd7d7270b43cac4094952977aba0f67308ea87
xlisachan <xlisachan@gmail.com>
2019-01-30 21:35:54 -0500

    Updates

---
+++ precache-manifest.4f3b0798b6e9919927b626bb5a46d799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c453baaf995628bb704",
+    "url": "/react_todolist/static/css/main.48cf110b.chunk.css"
+  },
+  {
+    "revision": "5c453baaf995628bb704",
+    "url": "/react_todolist/static/js/main.5c453baa.chunk.js"
+  },
+  {
+    "revision": "c5720035863c395f1347",
+    "url": "/react_todolist/static/js/1.c5720035.chunk.js"
+  },
+  {
+    "revision": "e5c2a2a7bace1dcc7750",
+    "url": "/react_todolist/static/js/runtime~main.e5c2a2a7.js"
+  },
+  {
+    "revision": "b7c06398dc4674d9fc8274b68e3e24f4",
+    "url": "/react_todolist/index.html"
+  }
+];
