b133418ba33f06b17319840124fd07b4385b756f
xlisachan <xlisachan@gmail.com>
2019-10-31 20:25:03 -0400

    Refactored BackgroundItem component

--- src/components/BackgroundItem.js
+++ src/components/BackgroundItem.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import '../stylesheets/BackgroundItem.css'
-import { FaGlobeAmericas } from 'react-icons/fa';
+import Phone from './Phone';
+import Passport from './Passport';
 
 const BackgroundItem = () => (
   <div className="background-items">
-      <div className="phone">
-        <div style={{width: '94%', display: 'flex', justifyContent: 'space-between'}}>
-          <div className="phone-camera1"></div>
-          <div className="phone-camera2"></div>
-        </div>
-        <div className="phone-screen"></div>
-      </div>
-      <div className="passport">
-        <div className="passport-book">
-          <div>PASSPORT</div>
-          <div className="passport-globe"><FaGlobeAmericas /></div>
-        </div>
-        <div className="passport-ticket">
-          <div className="passport-stripe"></div>
-        </div>
-      </div>
+    <Phone />
+    <Passport />
   </div>
 )
 
